9a66ea2a03849bc271e74dcdc3f720d744d40504
lordsteva <lordsteva@hotmail.com>
2019-02-07 22:24:16 +0100

    repozitorijum za rezervacije automobila

---
+++ projekat/src/main/java/isa/projekat/repository/CarReservationRepository.java
@@ -0,0 +1,28 @@
+package isa.projekat.repository;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import isa.projekat.model.Car;
+import isa.projekat.model.CarReservation;
+import isa.projekat.model.CarType;
+import isa.projekat.model.RentACarCompany;
+
+@Repository
+public interface CarReservationRepository extends JpaRepository<CarReservation, Long> {
+	
+	@Query("select cr from CarReservation cr "+
+			"where cr.startDate<=?2 and cr.endDate>=?1")
+	public List<CarReservation> getReport(Date start,Date end);
+	
+	@Query("select cr from CarReservation cr,Car c where "+
+			"cr.car=c and c.company=?1 and cr.endDate>=?2 and cr.endDate<=?3")
+	public List<CarReservation> getReservationsByCompany(RentACarCompany c, Date dstart, Date dend);
+	
+	
+}
